6da38f0b34875fcf3b7e59182489dea69329b0d4
Adrimi <adriano100417@gmail.com>
2020-02-13 13:02:28 +0100

    Working first navigation

--- HappyDays/HDCollectionViewController.swift
+++ HappyDays/HDCollectionViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
 
 class HDCollectionViewController: UICollectionViewController {
 
+    // MARK: - Lifecycle
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        checkPermissions()
+    }
     
+    // MARK: - Helper methods
+    private func checkPermissions() {
+        // chech status for all of the three permissions
+        let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
+        let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission == .granted
+        let transcribeAuthorized = SFSpeechRecognizer.authorizationStatus() == .authorized
+        
+        // make a single boolean out of all three
+        let authorized = photosAuthorized && recordingAuthorized && transcribeAuthorized
+        
+        // if we're missiong one, show the first run screen
+        if !authorized {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "PermissionsViewController") {
+                navigationController?.present(vc, animated: true)
+            }
+        }
+        
+    }
 
 }
+
