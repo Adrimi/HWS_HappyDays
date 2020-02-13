e3d2af1d34274eab4a3f6f25864adbb3f1e0cf3a
Adrimi <adriano100417@gmail.com>
2020-02-13 15:22:52 +0100

    Loading memories

--- HappyDays/HDCollectionViewController.swift
+++ HappyDays/HDCollectionViewController.swift
@@ -13,14 +13,50 @@ import Speech
 
 class HDCollectionViewController: UICollectionViewController {
 
+    //MARK: - Parametes
+    private(set) var memories = [URL]()
+    
     // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadMemories()
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         checkPermissions()
     }
     
+    //MARK: - Major mehtods
+    private func loadMemories() {
+        /// Remove any existing memories. Avoid duplication( set?)
+        memories.removeAll()
+        
+        /// Pull out a list of all the files stored in our app's documents directory. This needs to be working with URLs, rather than paths strings
+        guard let files = try? FileManager.default
+            .contentsOfDirectory(at: getDocumentsDirectory(),
+                                 includingPropertiesForKeys: nil,
+                                 options: []) else {return}
+        
+        /// Loop over every file that was found, and, if it was a thumbnail, add it to our memoeirs array/
+        for file in files where file.lastPathComponent.hasSuffix(".thumb") {
+            let noExtension = file.lastPathComponent.replacingOccurrences(of: ".thumb", with: "")
+            let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtension)
+            memories.append(memoryPath)
+        }
+        
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+    
     // MARK: - Helper methods
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
     private func checkPermissions() {
         // chech status for all of the three permissions
         let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
